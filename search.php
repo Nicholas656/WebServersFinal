012e17edabb4293b9cf1e87ef5a3132fdd59a67f
Nicholas656 <nicholas.goryczka@dawsoncollege.qc.ca>
2025-12-19 10:59:20 -0500

    First Run

---
+++ search.php
@@ -0,0 +1,18 @@
+<?php
+
+    #SQL setup
+    $server = "localhost";
+    $username = "php";
+    $password = "ThatDamnElephant";
+    $database = "GoogleLogs";
+
+    #Make the connection
+    $conn = mysqliconnect($server, $username, $password, $database);
+    if(!$conn)
+    {
+        #Error logging
+        die("Big problems are happening \nBetter yet, you can't even connect to the SQL database");
+    }
+
+    $sql = "SELECT * FROM Searches;";
+?>
