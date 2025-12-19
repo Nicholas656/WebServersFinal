eaf3343e1f1d6c648e296466a8a7c1bd27e89846
Nicholas656 <nicholas.goryczka@dawsoncollege.qc.ca>
2025-12-19 11:21:45 -0500

    Second Step?

--- search.php
+++ search.php
@@ -14,10 +14,19 @@
         die("Big problems are happening \nBetter yet, you can't even connect to the SQL database");
     }
 
-    $sql = "SELECT * FROM Searches;";
-    $result = mysqli_query($conn, $sql);
+    #POST request setup and handling
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $searchTerm = htmlspecialchars($_POST["search"]);
+        $IP = $_SERVER["REMOTE_ADDR"];
+    }
+    $command = "INSERT INTO Searches (IP, search) VALUES ('$IP', '$searchTerm')";
+
+    $result = mysqli_query($conn, $command);
     foreach($result as $row)
     {
         echo "<p>IP: {$row["IP"]}</p>";
     }
+
+    mysqli_close($conn);
 ?>
